59bc1a324bcf2a283545d367d49cef6878389da3
Dominique <dominique@chez-dominique.fr>
2015-04-21 11:00:28 +0200

    Mise à jour : Exercice 67 ok

--- app/src/main/java/lopezdominique/lecnamnfa024lopezdmy2048/MainActivity.java
+++ app/src/main/java/lopezdominique/lecnamnfa024lopezdmy2048/MainActivity.java
@@ -2,6 +2,8 @@ package lopezdominique.lecnamnfa024lopezdmy2048;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.LinearLayout;
 import android.widget.RatingBar;
 import android.widget.RelativeLayout;
@@ -173,5 +175,29 @@ public class MainActivity extends ActionBarActivity {
         boardLayoutParams.weight = wBoard;
         boardLayout.setLayoutParams(boardLayoutParams);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_new) {
+            game.init();
+            update();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
